79e18598d2e57d9aabe6ca2e857c880dca854da1
zerix807 <alan.turtleboy@gmail.com>
2018-11-21 10:56:34 -0700

    Added drone enable/disable script

---
+++ Assets/DroneStuff/Scripts/DroneEnableScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneEnableScript : MonoBehaviour {
+
+    public GameObject myDrone;
+    private bool isActive = false;
+
+    public void SwitchDrone()
+    {
+        isActive = !isActive;
+        myDrone.SetActive(isActive);
+    }
+}
